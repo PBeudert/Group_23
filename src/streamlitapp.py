0ea8ed817152bf6e309bf7db225a3cb3280e5ab6
Sebastian Frey <sc.frey@aol.de>
2025-02-27 16:07:59 +0000

    streamlit update

--- src/streamlitapp.py
+++ src/streamlitapp.py
@@ -2,8 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from MovieDataProcessor import MovieDataProcessor
-processor = MovieDataProcessor()
-N=st.number_input("What number of movie types would you like to see",value=10)
-st.write(processor.__movie_type__(10))
-st.title('Test1')
+
+st.title("Movie Data Processor")
+
+# Cache the processor so it doesn't reload on every UI interaction
+@st.cache_data
+def load_processor():
+    return MovieDataProcessor()
+
+processor = load_processor()
+
+# User input for number of movie types
+N = st.number_input("What number of movie types would you like to see", min_value=1, max_value=50, value=10, step=1)
+
+# Display movie types
+st.write("### Top Movie Types")
+st.dataframe(processor.__movie_type__(N))  # Use user-selected value
+
 
