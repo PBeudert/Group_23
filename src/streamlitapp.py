b8ddefb39ae23d09fb81c887ef5356ce072c4de6
Sebastian Frey <sc.frey@aol.de>
2025-02-27 16:00:28 +0000

    streamlit base

--- src/streamlitapp.py
+++ src/streamlitapp.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+from MovieDataProcessor import MovieDataProcessor
+processor = MovieDataProcessor()
+N=st.number_input("What number of movie types would you like to see",value=10)
+st.write(processor.__movie_type__(10))
 st.title('Test1')
 
