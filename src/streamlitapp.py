a572138ce5fb07e4dec58474dd84b974261559d7
Sebastian Frey <sc.frey@aol.de>
2025-02-27 17:10:16 +0000

    actor distribution try one

--- src/streamlitapp.py
+++ src/streamlitapp.py
@@ -38,3 +38,26 @@ ax.set_title("Histogram of Number_of_Actors")
 plt.xticks(rotation=45, ha="right")
 
 st.pyplot(fag)
+
+
+min_h = st.number_input("What is the plot minimum height", min_value=1.0, max_value=2.1, value=1.2, step=0.1)
+max_h = st.number_input("What is the plot maximum height", min_value=1.1, max_value=2.3, value=2.1, step=0.1)
+
+gender = st.selectbox("What gender are you interested in?;)",["All","Male","Female"])
+if gender == "Male":
+    gender="M"
+elif gender == "Female":
+    gender = "F"
+
+df_actor_distr=processor.__actor_distributions__(gender,max_h,min_h,False)
+
+fug, ax = plt.subplots(figsize=(10, 6))
+ax.bar(df_actor_distr["Height"], df_actor_distr["Count"], color="darkblue")
+ax.set_xlabel("Heights")
+ax.set_ylabel("Actor height occurences")
+ax.set_title("Histogram of Actor height distributions")
+ax.set_ylim(0, df_actor_distr["Count"].max() * 1.2)  # Adds 10% space above highest bar
+
+plt.xticks(rotation=90, ha="right")
+plt.tight_layout()
+st.pyplot(fug)
